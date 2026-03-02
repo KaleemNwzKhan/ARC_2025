b7944d1359cb50776b176bb4c4944c124176904d
KaleemNwzKhan <kk5271@rit.edu>
2026-03-02 16:12:01 -0500

    Upload run_edge_assist_docker.bash

---
+++ run_edge_assist_docker.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+xhost +local:root
+IMG=kk5271/rapid_v2v:1.0
+DATASET_DIRECTORY=/home/kk5271/Downloads/ARC/Dataset/
+CODEBASE_DIRECTORY=/home/kk5271/Downloads/ARC/
+# Check if directories that we want to mount exist
+if [ ! -d "$DATASET_DIRECTORY" ]; then
+        echo "The dataset directory at $DATASET_DIRECTORY does not exist"
+        return 1
+fi
+
+if [ ! -d "$CODEBASE_DIRECTORY" ]; then
+        echo "The code base directory at $CODEBASE_DIRECTORY does not exist"
+        return 1
+fi
+# If NVIDIA is present, use Nvidia-docker
+if test -c /dev/nvidia0
+then
+    docker run --rm -it \
+      --runtime=nvidia \
+      --privileged \
+      --device /dev/dri:/dev/dri \
+      --env="DISPLAY" \
+      --env="QT_X11_NO_MITSHM=1" \
+      -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+      -v "$DATASET_DIRECTORY:/dataset:rw" \
+      -v "$CODEBASE_DIRECTORY:/workspace:rw" \
+      --name rapid_v2v \
+      --network host \
+      $IMG \
+      bash
+else
+    docker run --rm -it \
+      -e DISPLAY \
+      --device=/dev/dri:/dev/dri \
+      -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+      -v "$DATASET_DIRECTORY:/dataset:rw" \
+      -v "$CODEBASE_DIRECTORY:/workspace:rw" \
+      --name rapid_v2v \
+      $IMG \
+      bash
+fi
